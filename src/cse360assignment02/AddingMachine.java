c562b4c032f97f0c6b10a2b9d21ec4d1d1f41fd8
Tristan Hoffay <tristanthoffay@gmail.com>
2020-10-05 21:37:34 -0700

    Completed methods and documentation

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,44 @@
 package cse360assignment02;
 
 public class AddingMachine {
+
+    // Current value stored by the adding machine
     private int total;
+    // String displaying history of operations on the total (Example: "0 + 2 - 3 + 4")
+    private String history;
 
+
+    // Constructor initializes total as 0 and history as a String starting with 0
     public AddingMachine () {
-        total = 0;  // not needed - included for clarity
+        total = 0;
+        history = "" + total;
     }
 
+    // Returns the current total of the AddingMachine object
     public int getTotal () {
-        return 0;
+        return total;
     }
 
+    // Increments the total by the input value and appends the operation to the history String
     public void add (int value) {
+        total += value;
+        history += " + " + value;
     }
 
+    // Subtracts the input value from the total and appends the operation to the history String
     public void subtract (int value) {
+        total -= value;
+        history += " - " + value;
     }
 
+    // Returns the recorded history of operations to the total
     public String toString () {
-        return "";
+        return history;
     }
 
+    // Resets the total to 0 and erases the operation history
     public void clear() {
+        total = 0;
+        history = "" + total;
     }
 }
